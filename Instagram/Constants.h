4033f3f078f75d6738f31f664e56d80a7ef2dc65
Saulo Arruda <sauloarruda@gmail.com>
2012-07-25 18:58:25 -0400

    upload do arquivo da imagem

--- Instagram/Constants.h
+++ Instagram/Constants.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 Jera. All rights reserved.
 //
 
-#define API_ROOT @"https://raw.github.com/sauloarruda/Instagram/master"
+#define API_ROOT @"http://192.168.20.136/~sauloarruda/api/v1"
+//#define API_ROOT @"https://raw.github.com/sauloarruda/Instagram/master"
 
 #define TIMELINE_URL_FORMAT @"%@/timeline.json"
+#define UPLOAD_URL_FORMAT @"%@/upload.json"
---
+++ Instagram/UploadViewController.h
@@ -0,0 +1,13 @@
+//
+//  UploadViewController.h
+//  Instagram
+//
+//  Created by Saulo Arruda Coelho on 7/25/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UploadViewController : UIViewController<UINavigationControllerDelegate, UIImagePickerControllerDelegate, NSURLConnectionDelegate, NSURLConnectionDataDelegate>
+
+@end
